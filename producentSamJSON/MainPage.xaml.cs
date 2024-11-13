a38b73baef20e4f4b9bdde14f9b54eb5c6a9ef57
5m <konradek982@gmail.com>
2024-11-13 11:10:04 +0100

    Brand Data Base Download Working

--- producentSamJSON/MainPage.xaml.cs
+++ producentSamJSON/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace producentSamJSON
+﻿using System.Text.Json;
+
+namespace producentSamJSON
 {
     public partial class MainPage : ContentPage
     {
@@ -14,7 +16,6 @@
             public int Model_ID { get; set; }
             public string? Model_Name { get; set; }
         }
-
         public class Output
         {
             public int Count { get; set; }
@@ -22,20 +23,35 @@
             public string? StringCriteria { get; set; }
             public List<Models> Results { get; set; }
         }
+
+
         private async void onDataDownloadClicked(object sender, EventArgs e)
         {
             string content = "";
-            var Items =
+            Output items = new Output();
             HttpClient client = new HttpClient();
-            string URL = "https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/saab?format=json";
+            string URL = $"https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/{entMarka.Text}?format=json";
+
             HttpResponseMessage response = await client.GetAsync(URL);
             if (response.IsSuccessStatusCode)
             {
                 content = await response.Content.ReadAsStringAsync();
-                
+                items = JsonSerializer.Deserialize<Output>(content);
             }
-            lblDane.Text = content;
-             //Models modelOutput = JsonSerializer.Deserialize();
+
+            if (items.Results != null)
+            {
+                lblDane.Text = $"Marka: {items.Results[0].Make_Name} (id: {items.Results[0].Make_ID}) \n\nModele:\n";
+                foreach (Models model in items.Results)
+                {
+                    lblDane.Text += $"{model.Model_Name} (id: {model.Model_ID}) \n";
+                }
+            }
+            else
+            {
+                lblDane.Text = "Nazwa marki nie poprawnie wpisana";
+            }
+            
         }
     }
 
